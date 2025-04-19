035abab5d8b3b6ec5e9a21eae84ea65f1454fe91
sunny3187 <atthesunny@gmail.com>
2025-04-19 23:20:57 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+num1=input("Enter First number: ")
+num2=input("Enter second number: ")
+num1=int(num1)
+num2=int(num2)
+print(f"{num1} + {num2} = {num1+num2}")
+print(f"{num1} - {num2} = {num1-num2}")
+print(f"{num1} * {num2} = {num1*num2}")
+print(f"{num1} / {num2} = {num1/num2}")
+
---
+++ task2.py
@@ -0,0 +1,3 @@
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+print(f"Hello, {first} {last}! welcome to the Python program.")
